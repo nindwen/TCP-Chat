d658966dc8fe1a62cc7584f93291f79748d40378
nindwen <me@nindwen.blue>
2016-06-21 19:39:47 +0300

    Clean the code

--- main.c
+++ main.c
@@ -7,9 +7,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define LOG_FILE "usvs.log"
+
 void handleConnection(int, int*, int);
 void serverLoop(int*);
 void sendToClient(int);
+
 void error(const char *msg)
 {
       perror(msg);
@@ -18,8 +21,10 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+      //Ignore child signals
       signal(SIGCHLD,SIG_IGN);
 
+      //Socket init
       int sockfd, newsockfd, portno, pid;
       socklen_t clilen;
       struct sockaddr_in serv_addr, cli_addr;
@@ -27,6 +32,7 @@ int main(int argc, char *argv[])
       sockfd = socket(AF_INET, SOCK_STREAM, 0);
       if (sockfd < 0) 
             error("ERROR opening socket");
+
       bzero((char *) &serv_addr, sizeof(serv_addr));
       portno = 5650;
       serv_addr.sin_family = AF_INET;
@@ -77,11 +83,8 @@ int main(int argc, char *argv[])
       return 0; /* we never get here */
 }
 
-/******** handleConnection() *********************
-  There is a separate instance of this function 
-  for each connection.  It handles all communication
-  once a connnection has been established.
- *****************************************/
+//Forks to sendToClient. Reads messages from children and
+//sends them to serverLoop through pipe
 void handleConnection (int sock, int* pipe, int maxClientId)
 {
       switch(fork()) {
@@ -96,6 +99,8 @@ void handleConnection (int sock, int* pipe, int maxClientId)
       int n;
       char buffer[256];
       char with_nick[256];
+      bzero(buffer,256);
+      bzero(with_nick,256);
 
       while(read(sock,buffer,255-strlen(with_nick))) {
             sprintf(with_nick, "[%5d]: ", maxClientId);
@@ -106,9 +111,10 @@ void handleConnection (int sock, int* pipe, int maxClientId)
       }
 }
 
+//Reads from file, writes to clients
 void sendToClient(int sock) {
       FILE* handle = NULL;
-      handle = fopen("usvs.log","r");
+      handle = fopen(LOG_FILE,"r");
       if(handle == NULL) {
             perror("Cannot open log (r)");
       }
@@ -121,12 +127,13 @@ void sendToClient(int sock) {
       }
 }
 
+//Listens to children, writes to file
 void serverLoop(int* pipe) {
       close(pipe[1]);
       int n;
       char buffer[256];
       FILE* handle = NULL;
-      handle = fopen("usvs.log","a");
+      handle = fopen(LOG_FILE,"a");
       if(handle == NULL) {
             perror("Cannot open log (r)");
       }
